c98b5c1408711b471934274770f5ce0e57d7dd68
della <della@Della-PC>
2024-08-28 18:36:48 +0530

    commit

--- testmaven/src/main/java/maventest/Sampletest.java
+++ testmaven/src/main/java/maventest/Sampletest.java
@@ -1,11 +1,12 @@
 package maventest;
 
-public class Sampletest {
+public class Sampletest 
+{
 
 	public static void main(String[] args) 
 	{
 		
-	System.out.println();
+	System.out.println("Hello");
 
 	}
 
